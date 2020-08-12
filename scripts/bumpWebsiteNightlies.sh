926a66d1ea1c1d84bb2d3a6c5552abab3b043ae6
Dominika <sokolov.dominika@gmail.com>
2020-08-12 18:37:50 -0400

    Added release count API

---
+++ scripts/bumpWebsiteNightlies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Created by dominikaaaa on 12/08/20 at 18:12
+
+COUNTER="$(grep total_nightly_releases _config.yml)"
+COUNTER=${COUNTER:24} # remove beginning 'total_nightly_releases: ' from variable
+COUNTER=$(($COUNTER+1))
+
+sed -i "s/total_nightly_releases: .*/total_nightly_releases: ${COUNTER}/" _config.yml
+
+echo "Bumped release count to: $COUNTER"
